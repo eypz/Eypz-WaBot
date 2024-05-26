afce44d250e08097d446bd7ffc23ee1aed39792f
Eypz God <168025354+eypz@users.noreply.github.com>
2024-05-26 15:20:54 +0530

    Update yt-search.js

--- commandes/yt-search.js
+++ commandes/yt-search.js
@@ -19,9 +19,9 @@ zokou({ nomCom: "yts", categorie: "Search", reaction: "🐅" }, async (dest, zk,
 
     let captions = "";
 for (let i = 0; i < 15; i++) {
-  captions += `╭━━⊱𝗛𝗔𝗖𝗞𝗜𝗡𝗚-𝗠𝗗⊱━━╮\n${i + 1}. Title: ${resultat[i].title}\nTime : ${resultat[i].timestamp}\nUrl: ${resultat[i].url}\n`;
+  captions += `╭━━⊱Eypz-WaBot⊱━━╮\n${i + 1}. Title: ${resultat[i].title}\nTime : ${resultat[i].timestamp}\nUrl: ${resultat[i].url}\n`;
 }
-    captions += "\n======\n*powered by hacking-Md*";
+    captions += "\n======\n*powered by Eypz-WaBot*";
 
     // repondre(captions)
     zk.sendMessage(dest, { image: { url: resultat[0].thumbnail }, caption: captions }, { quoted: ms });
